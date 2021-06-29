96c7418e60f6d29ccb9b951d5e7078f3a7578c46
Adeline BENOIT <adbenoit@e1r11p14.42.fr>
2021-06-29 16:16:19 +0200

    comment++

--- Exam_rank_04/main.c
+++ Exam_rank_04/main.c
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 13:49:50 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/06/29 15:19:25 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/06/29 16:14:47 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 
 int main(int ac, char **av, char *envp[])
 {
-    int i;
-    int start;
+	int i;
+	int start;
 
-    if (ac < 2)
-        return (0);
-    i = 1;
-    start = 1; // skip a.out
-    while (av[i])
-    {
-        if (strcmp(av[i], ";") == 0)
-        {
-            av[i] = 0; // to use the av until ";" => one command
-            lauch_cmd(av + start, envp);
-            ++i;
-            while (av[i] && strcmp(av[i], ";") == 0)
-               ++i;
-            start = i; // next command is after ";"
-        }
-        else
-           ++i;
-    }
-    if (av[start])
-        lauch_cmd(av + start, envp);
-    return (0);
+	if (ac < 2)
+		return (0);
+	i = 1;
+	start = 1;										// skip a.out
+	while (av[i])
+	{
+		if (strcmp(av[i], ";") == 0)
+		{
+			av[i] = 0; 								// to use av until ";" => one command
+			lauch_cmd(av + start, envp);
+			++i;
+			while (av[i] && strcmp(av[i], ";") == 0)
+			   ++i;
+			start = i;								// next command => after ";"
+		}
+		else
+		   ++i;
+	}
+	if (av[start])
+		lauch_cmd(av + start, envp);				//launch last command
+	return (0);
 }
  
--- Exam_rank_04/microshell.c
+++ Exam_rank_04/microshell.c
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 15:09:39 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/06/29 16:05:58 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/06/29 16:15:42 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void    ft_cd(char **arg)
 {
-	if (!arg[1] || arg[2]) // check the number of arguments
+	if (!arg[1] || arg[2])                          // check the number of arguments
 		ft_strerror("error: cd: bad arguments", NULL);
 	else if (chdir(arg[1]) == -1)
 		ft_strerror("error: cd: cannot change directory to ", arg[1]);
@@ -22,15 +22,15 @@ void    ft_cd(char **arg)
 
 void    child_process(char **arg, int *fd, int fd_in, int last, char *envp[])
 {
- 		if (dup2(fd_in, STDIN_FILENO) == -1) // reading the output of the previous command
- 			ft_strerror("error: fatal", NULL);
+ 		if (dup2(fd_in, STDIN_FILENO) == -1)        // reading the output of the previous command
+ 			ft_strerror("error: fatal", NULL); 
  		if (last == 0)
- 			if (dup2(fd[1], STDOUT_FILENO) == -1) // will be the input of the next commmand
+ 			if (dup2(fd[1], STDOUT_FILENO) == -1)   // will be the input of the next commmand
      			ft_strerror("error: fatal", NULL);
  		close(fd_in);
  		close(fd[0]);
  		close(fd[1]);
- 		if (execve(arg[0], arg, envp) == -1) //execute command
+ 		if (execve(arg[0], arg, envp) == -1)        //execute command
  			ft_strerror("error: cannot execute ", arg[0]);
 }
 
@@ -45,16 +45,16 @@ int    ft_execute(char **arg, char *envp[], int fd_in)
 	last = 0;
 	while (arg[i] && strcmp(arg[i], "|"))
 		++i;
-	if (!arg[i]) // is this the last pipe ?
+	if (!arg[i])                                // is this the last pipe ?
 		last = 1;
-	arg[i] = 0; // to execute command until "|"
+	arg[i] = 0;                                 // to execute command until "|"
 	if (pipe(fd) == -1 || (pid = fork()) == -1)
 	    ft_strerror("error: fatal", NULL);
 	else if (pid == 0)
        child_process(arg, fd, fd_in, last, envp);
 	else
 	{
-		if (dup2(fd[0], fd_in) == -1) // save fd[0] for the next command
+		if (dup2(fd[0], fd_in) == -1)           // save fd[0] for the next command
    		    ft_strerror("error: fatal", NULL);
 		close(fd[0]);
 		close(fd[1]);
@@ -73,7 +73,7 @@ void    launch_cmd(char **arg, char *envp[])
 
     if (!strcmp(arg[0], "cd"))
 			return (ft_cd(arg));
-	if ((fd_in = dup(STDIN_FILENO)) == -1) // duplicate the fd of the stdin
+	if ((fd_in = dup(STDIN_FILENO)) == -1)      // duplicate the fd of the stdin
 	    ft_strerror("error: fatal", NULL);
 	i = 0;
 	while (arg[i])
