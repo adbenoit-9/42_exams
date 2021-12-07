a2056ef6941612a9600950bd4b60d8f3658542da
Adeline BENOIT <adbenoit@e1r12p4.42.fr>
2021-12-07 16:16:42 +0100

    exam ok

--- Exam_rank_06/mini_serv.c
+++ Exam_rank_06/mini_serv.c
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 13:50:09 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/12/03 17:12:49 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/12/07 14:35:54 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,26 +34,11 @@ typedef struct s_fds
 	struct fd_set	all;
 	struct fd_set	read;
 	struct fd_set	write;
+	int				socket;
 }				t_fds;
 
-void	exit_error(char *str, t_client *client)
-{
-	t_client	*it;
 
-	it = client;
-	while (client)
-	{
-		it = it->next;
-		close(client->fd);
-		free(client->buffer);
-		free(client);
-		client = it;
-	}
-	write(STDERR_FILENO, str, strlen(str));
-	write(STDERR_FILENO, "\n", 1);
-	exit(1);
-}
-
+/**** begin of subject functions ****/
 int extract_message(char **buf, char **msg)
 {
 	char	*newbuf;
@@ -100,6 +85,29 @@ char *str_join(char *buf, char *add)
 	strcat(newbuf, add);
 	return (newbuf);
 }
+/**** end of subject funtions ****/
+
+
+/* Clear client_lst and close fds */
+void	exit_error(char *str, t_client *client_lst, int sockfd)
+{
+	t_client	*tmp;
+
+	tmp = client_lst;
+	while (client_lst)
+	{
+		tmp = tmp->next;
+		close(client_lst->fd);
+		free(client_lst->buffer);
+		free(client_lst);
+		client_lst = tmp;
+	}
+	if (sockfd != -1)
+		close(sockfd);
+	write(STDERR_FILENO, str, strlen(str));
+	write(STDERR_FILENO, "\n", 1);
+	exit(1);
+}
 
 /* Send the message to all clients */
 void	send_message(char *msg, t_client *client_lst, t_client *sender,
@@ -117,101 +125,98 @@ void	send_message(char *msg, t_client *client_lst, t_client *sender,
 /* Adds a client to the list and informs the others of his arrival. */
 t_client	*add_newclient(t_client *client_lst, int fd, int id, t_fds *fds)
 {
-	t_client	*it;
+	t_client	*newcli, *last;
 	char		str[4096];
 
-	FD_SET(fd, &fds->all);
-	if (!client_lst)
+	
+	newcli = (t_client *)malloc(sizeof(t_client));
+	if (!newcli)
 	{
-		client_lst = (t_client *)malloc(sizeof(t_client));
-		if (!client_lst)
-		{
-			close(fd);
-			exit_error("Fatal Error", NULL);
-		}
-		client_lst->id = id;
-		client_lst->fd = fd;
-		client_lst->buffer = NULL;
-		client_lst->next = NULL;
-		send_message("server: client 0 just arrived\n", client_lst, client_lst, fds);
-		return (client_lst);
+		close(fd);
+		exit_error("Fatal Error", client_lst, fds->socket);
 	}
-	/* get the last client */
-	it = client_lst;
-	while (it->next)
-		it = it->next;
+	newcli->id = id;
+	newcli->fd = fd;
+	newcli->buffer = NULL;
+	newcli->next = NULL;
+	FD_SET(fd, &fds->all);
+	sprintf(str, "server: client %d just arrived\n", id);
+	send_message(str, client_lst, newcli, fds);
+	if (!client_lst)
+		return (newcli);
 	/* adds the new client at the back of the list */
-	it->next = (t_client *)malloc(sizeof(t_client));
-	if (!it->next)
-		exit_error("Fatal Error", client_lst);
-	it->next->id = id;
-	it->next->fd = fd;
-	it->next->buffer = NULL;
-	it->next->next = NULL;
-	sprintf(str, "server: client %d just arrived\n", it->next->id);
-	send_message(str, client_lst, it->next, fds);
+	last = client_lst;
+	while (last->next)
+		last = last->next;
+	last->next = newcli;
 	return (client_lst);
 }
 
 /* Removes a client from the list, disconnect him and informs the others. */
-t_client	*remove_client(t_client *client_lst, t_client *del, t_fds *fds)
+t_client	*remove_client(t_client *client_lst, t_client *rmcli, t_fds *fds)
 {
 	char		str[4096];
-	t_client	*it;
+	t_client	*prev;
 
-	it = client_lst;
-	while (it && it->next != del)
-		it = it->next;
-	sprintf(str, "server: client %d just left\n", del->id);
-	send_message(str, client_lst, del, fds);
-	FD_CLR(del->fd, &fds->all);
-	close(del->fd);
-	if (client_lst != del)
-		it->next = del->next;
+	if (client_lst == rmcli)
+		client_lst = rmcli->next;
 	else
-		client_lst = client_lst->next;
-	free(del);
+	{
+		prev = client_lst;
+		while (prev->next != rmcli)
+			prev = prev->next;
+		prev->next = rmcli->next;
+	}
+	sprintf(str, "server: client %d just left\n", rmcli->id);
+	send_message(str, client_lst, rmcli, fds);
+	FD_CLR(rmcli->fd, &fds->all);
+	close(rmcli->fd);
+	free(rmcli->buffer);
+	free(rmcli);
 	return (client_lst);
 }
 
 /* Check if the server receive a message and resend it to all clients. */
-t_client	*receive_message(t_client *client, t_fds *fds)
+t_client	*receive_message(t_client *client_lst, t_fds *fds)
 {
 	char		buffer[BUFFER_SIZE];
 	char		*msg;
 	char		str[5000];
 	int			ret;
-	t_client	*it, *tmp;
+	t_client	*it, *curr_cli;
 
-	it = client;
+	it = client_lst;
 	while (it)
 	{
-		/* message receives from it */
-		if (FD_ISSET(it->fd, &fds->read))
+		curr_cli = it;
+		it = curr_cli->next;
+		/* message receives from curr_cli */
+		if (FD_ISSET(curr_cli->fd, &fds->read))
 		{
-			ret = recv(it->fd, buffer, BUFFER_SIZE - 1, MSG_WAITALL);
-			tmp = it;
-			it = it->next;
+			ret = recv(curr_cli->fd, buffer, BUFFER_SIZE - 1, 0);
 			/* client disconnects */
 			if (ret == 0)
-				client = remove_client(client, tmp, fds);
+				client_lst = remove_client(client_lst, curr_cli, fds);
 			/* send messsage */
 			else if (ret != -1)
 			{
 				buffer[ret] = 0;
-				tmp->buffer = str_join(tmp->buffer, buffer);
-				while (extract_message(&tmp->buffer, &msg))
+				curr_cli->buffer = str_join(curr_cli->buffer, buffer);
+				if (!curr_cli->buffer)
+					exit_error("Fatal Error", client_lst, fds->socket);
+				while ((ret = extract_message(&curr_cli->buffer, &msg)))
 				{
-					sprintf(str, "client %d: %s", tmp->id, msg);
-					send_message(str, client, tmp, fds);
+					sprintf(str, "client %d: %s", curr_cli->id, msg);
+					send_message(str, client_lst, curr_cli, fds);
 					free(msg);
 				}
+				if (ret == -1)
+					exit_error("Fatal Error", client_lst, fds->socket);
+					
 			}
 		}
-		else
-			it = it->next;
 	}
-	return (client);
+	return (client_lst);
 }
 
 /* Waits for a connection on the server and processes it. */
@@ -221,11 +226,12 @@ void	handle_connection(int sockfd)
 	struct sockaddr_in	addr;
 	t_fds				fds;
 	socklen_t			len;
-	t_client			*client;
+	t_client			*client_lst;
 
+	fds.socket = sockfd;
 	FD_ZERO(&fds.all);
 	FD_SET(sockfd, &fds.all);
-	client = NULL;
+	client_lst = NULL;
 	maxfd = sockfd;
 	client_id = 0;
 	while (1)
@@ -236,22 +242,22 @@ void	handle_connection(int sockfd)
 		ret = select(maxfd + 1, &fds.read, &fds.write, NULL, NULL);
 		/* error case */
 		if (ret == -1)
-			exit_error("Fatal Error", NULL);
+			exit_error("Fatal Error", NULL, fds.socket);
 		/* connection founds */
 		else if (ret > 0 && FD_ISSET(sockfd, &fds.read))
 		{
 			len = sizeof(struct sockaddr_in);
 			connfd = accept(sockfd, (struct sockaddr *)&addr, &len);
-			// fcntl(connfd, F_SETFL, O_NONBLOCK);
+			fcntl(connfd, F_SETFL, O_NONBLOCK);
 			if (connfd > 0)
 			{
-				client = add_newclient(client, connfd, client_id, &fds);
+				client_lst = add_newclient(client_lst, connfd, client_id, &fds);
 				maxfd = connfd > maxfd ? connfd : maxfd;
 				++client_id;
 			}
 		}
-		else if (client && ret > 0)
-			client = receive_message(client, &fds);
+		else if (client_lst && ret > 0)
+			client_lst = receive_message(client_lst, &fds);
 	}
 }
 
@@ -261,21 +267,20 @@ int	main(int ac, char **av)
 	struct sockaddr_in	servaddr;
 
 	if (ac < 2)
-		exit_error("Wrong number of arguments", NULL);
+		exit_error("Wrong number of arguments", NULL, -1);
 	port = atoi(av[1]);
 	/* Setup server */
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1)
-		exit_error("Fatal Error", NULL);
+		exit_error("Fatal Error", NULL, -1);
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(2130706433); // 127.0.0.1
 	servaddr.sin_port = htons(port);
 	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)))
-		exit_error("Fatal Error", NULL);
+		exit_error("Fatal Error", NULL, sockfd);
 	if (listen(sockfd, SOMAXCONN) != 0)
-		exit_error("Fatal Error", NULL);
+		exit_error("Fatal Error", NULL, sockfd);
 	handle_connection(sockfd);
-	close(sockfd);
-	exit(0);
+	return (0);
 }
